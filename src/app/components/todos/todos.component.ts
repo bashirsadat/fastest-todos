ba2a8d415228103f3088188258e57faa0d840cec
bashir sadat <sadat.sayedbashir@gmail.com>
2022-02-23 12:39:28 -0500

    npm versionfddfasdfadf

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -7,6 +7,7 @@ import {Todo} from './../../models/Todo';
 })
 export class TodosComponent implements OnInit {
   todos:Todo[];
+  inputTodo:string="";
   constructor() { }
 
   ngOnInit(): void {
@@ -32,6 +33,15 @@ export class TodosComponent implements OnInit {
     }
     deleteTodo (id:number) {
       this.todos= this.todos.filter((v,i) => i !==id);
+    }
+    addTodo (){
+    
+      this.todos.push({
+        content:this.inputTodo,
+        completed:false
+  
+      })
+      this.inputTodo="";
     }
     
 
